fde6f4b9e601c8dd7c8881cb40669e8f3a16a239
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-14 16:56:35 -0700

    creatig profile component to link to header nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function SideNav(){
     return <nav className="sticky top-0 px-2 py-4">
         <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
             <li>
-                <Link></Link>
+                <Link href='/'>Home</Link>
+            </li>
+            <li>
+                <Link href={`/profiles/`}>Profile</Link>
             </li>
         </ul>
     </nav>
