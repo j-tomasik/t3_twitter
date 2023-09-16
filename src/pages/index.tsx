c5641b874f7076881eec47c55ba910ee78ff6802
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-15 17:21:14 -0700

    main body home styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import Link from "next/link";
 
-// import { api } from "~/utils/api";
+
 
 export default function Home() {
-  return <h1>Hiiii</h1>
+  return <>
+    <header className='sticky top-0 z-10 border-b bg-white pt-2'>
+      <h1 className='mb-2 px-4 text-lg font-bold'>Home</h1>
+    </header>
+  </>
 }
 
-// function AuthShowcase() {
-//   const { data: sessionData } = useSession();
 
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// }
