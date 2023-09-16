da2915a0990c3c5d1d8fe299f2f86376e6fa2a13
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-15 17:44:25 -0700

    custom button with variable css

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react"
+
+type ButtonProps = {
+    small?: boolean
+    gray?: boolean
+    className?: string
+} & DetailedHTMLProps<
+ButtonHTMLAttributes<HTMLButtonElement>,
+HTMLButtonElement
+>;
+
+export function Button({small = false, gray = false, className="", ...props }: ButtonProps) {
+    const sizeClasses = small ? 'px-2 py-1' : 'px-4 py-2 font-bold'
+
+    return <button {...props}></button>
+}
--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,5 +1,12 @@
-export default function NewTweetForm() {
-    return <form className='flex flex-col'>
+import { Button } from "./Button";
 
+export default function NewTweetForm() {
+    return <form className='flex flex-col gap-2 border-b px-4 py-2'>
+        <div className='flex gap-4 '>
+            {/* <ProfileImage src='u3rl'/> */}
+            <textarea className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
+            placeholder="What's happening?"/>
+        </div>
+        <Button>Tweet</Button>
     </form>
 }
