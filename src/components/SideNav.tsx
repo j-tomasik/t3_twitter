63767fd88df09bb3ba9950b014394bc077d3f728
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-15 17:09:27 -0700

    adding sign out and sign in buttons

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,14 +1,26 @@
+import {signOut, useSession} from 'next-auth/react';
 import Link from 'next/link';
 
 export default function SideNav(){
+    const session = useSession()
+    const user = session.data?.user;
+
     return <nav className="sticky top-0 px-2 py-4">
         <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
             <li>
                 <Link href='/'>Home</Link>
             </li>
-            <li>
-                <Link href={`/profiles/`}>Profile</Link>
-            </li>
+            {user != null && (
+                <li>
+                    <Link href={`/profiles/${user.id}`}>Profile</Link>
+                </li>
+            )}
+            {user != null && (
+                <li>
+                    <button onClick={void signOut}>Log Out</button>
+                </li>
+            )}
+            {user == null ? null : null}
         </ul>
     </nav>
 }
