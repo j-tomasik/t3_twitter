b978d1a855dec4f6c03fc7340faf30dda432bb4c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-15 18:13:20 -0700

    building out profile image component

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,9 +1,9 @@
 import { Button } from "./Button";
-
+import { ProfileImage} from "./ProfileImage";
 export default function NewTweetForm() {
     return <form className='flex flex-col gap-2 border-b px-4 py-2'>
         <div className='flex gap-4 '>
-            {/* <ProfileImage src='u3rl'/> */}
+            <ProfileImage src='#url'/>
             <textarea className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
             placeholder="What's happening?"/>
         </div>
