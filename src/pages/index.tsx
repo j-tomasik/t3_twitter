74e8574c49412eb9cd2120fb6a66fa788c178a98
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-18 12:52:04 -0700

    creating component for tweet stream

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {NewTweetForm} from '../components/NewTweetForm'
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 
 
 
@@ -8,8 +9,15 @@ export default function Home() {
     <header className='sticky top-0 z-10 border-b bg-white pt-2'>
       <h1 className='mb-2 px-4 text-lg font-bold'>Home</h1>
       <NewTweetForm />
+      <RecentTweets />
     </header>
   </>
+};
+
+function RecentTweets () {
+  const tweets = []
+
+  return <InfiniteTweetList tweets={tweets}/>
 }
 
 
