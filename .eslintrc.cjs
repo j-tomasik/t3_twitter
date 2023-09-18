c41db71fb82b45dfefc795c821b5ef2543fb7a08
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-18 11:23:16 -0700

    Setting up tweet router in api

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,8 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
+    "@typescript-eslin/no-unsafe-call": "off",
+    "@typescript-eslin/no-unsafe-return": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
