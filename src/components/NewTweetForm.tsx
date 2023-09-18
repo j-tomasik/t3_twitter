a250dbeab5bd6d6775d807b726f5e93ce7e2648d
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-18 11:59:48 -0700

    trying to track down prisma create mutaute bug

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,20 +1,21 @@
+import { api } from "~/utils/api";
 import { Button } from "./Button";
 import { ProfileImage} from "./ProfileImage";
 import { useSession } from "next-auth/react";
-import { useLayoutEffect, useState, useRef, useCallback } from 'react';
+import { useLayoutEffect, useState, useRef, useCallback, FormEvent } from 'react';
 
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
     if (textArea == null) return
     textArea.style.height = "0"
-    textArea.style.height = `${textArea.scrollHeight}px`
+    textArea.style.height = `${textArea.scrollHeight}px`;
 }
 
 export function NewTweetForm() { 
     const session = useSession();
     if (session.status !== "authenticated") return null;
 
-    return <Form />
+    return <Form />;
 }
 
 
@@ -24,25 +25,38 @@ function Form() {
     const [inputValue, setInputValue] = useState('');
     const textAreaRef = useRef<HTMLTextAreaElement>()
 
-    const inputRef = useCallback((textArea:HTMLTextAreaElement) => {
+    const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
         updateTextAreaSize(textArea);
         textAreaRef.current = textArea
     }, [])
     
     useLayoutEffect(() => {
         updateTextAreaSize(textAreaRef.current)
-    }, [inputValue])
+    }, [inputValue]);
+
+    const createTweet = api.tweet.create.useMutation({
+        onSuccess: (newTweet) => {
+        console.log(newTweet)
+        setInputValue('')
+        },
+        });
+
+    function handleSubmit (e: FormEvent) {
+        e.preventDefault()
+
+        createTweet.mutate({content: inputValue})
+    }
     
-    if (session.status !== "authenticated") return 
+    if (session.status !== "authenticated") return null;
     
-    return <form className='flex flex-col gap-2 border-b px-4 py-2'>
+    return <form onSubmit={handleSubmit} className='flex flex-col gap-2 border-b px-4 py-2'>
         <div className='flex gap-4 '>
             <ProfileImage src={session.data.user.image}/>
             <textarea 
             ref={inputRef}
             style={{height: 0}}
             value={inputValue}
-            onChange={e => setInputValue(e.target.value)}
+            onChange={(e) => setInputValue(e.target.value)}
             className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
             placeholder="What's happening?"/>
         </div>
