c48937d04af307b2268e24885c3ce1c50084e8b7
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-18 10:23:43 -0700

    using state from use ref for text area sizing

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,34 @@
 import { Button } from "./Button";
 import { ProfileImage} from "./ProfileImage";
 import { useSession } from "next-auth/react";
+import { useEffect, useState, useRef } from 'react';
+
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+    if (textArea == null) return
+    textArea.style.height = "0"
+    textArea.style.height = `${textArea.scrollHeight}px`
+}
+
 export default function NewTweetForm() { 
     const session = useSession()
-
-    if (session.status !== "authenticated") return 
-
+    const [inputValue, setInputValue] = useState('');
+    const textAreaRef = useRef<HTMLTextAreaElement>()
     
-
+    useEffect(() => {
+        updateTextAreaSize(textAreaRef.current)
+    }, [inputValue])
+    
+    if (session.status !== "authenticated") return 
+    
     return <form className='flex flex-col gap-2 border-b px-4 py-2'>
         <div className='flex gap-4 '>
             <ProfileImage src={session.data.user.image}/>
-            <textarea className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
+            <textarea 
+            style={{height: 0}}
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
             placeholder="What's happening?"/>
         </div>
         <Button className="self-end">Tweet</Button>
