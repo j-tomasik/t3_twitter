0ba06c0614666463a95ecfea3b8d2dd2b8434ead
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-19 16:37:32 -0700

    Styling internal elements

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -42,5 +42,18 @@ export function InfiniteTweetList ({tweets,  isError, isLoading, fetchNewTweets,
 }
 
 function TweetCard({id, user, content, createdAt, likeCount, likedByMe}: Tweet) {
-    return <li className='flex gap-4 border-b px-4 py-4'><Link href={`/profiles/${user.id}`}><ProfileImage src={user.image}/></Link></li>
+    return <li className='flex gap-4 border-b px-4 py-4'>
+        <Link href={`/profiles/${user.id}`}>
+            <ProfileImage src={user.image}/></Link>
+            <div className="flex flex-grow flex-col">
+                <div className='flex gap-1'>
+                    <Link 
+                        href={`/profiles/${user.id}`}
+                        className="font-bold hover:underline"
+                    >
+                    {user.name}
+                    </Link>
+                </div>
+            </div>
+            </li>
 }
