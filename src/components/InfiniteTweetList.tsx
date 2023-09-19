bd3ec0d72a771df8907aa530c7197bf5fbcffbdb
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-19 15:52:08 -0700

    using infinite scroll componnent from lib to map tweets

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,3 +1,5 @@
+import InfiniteScroll from "react-infinite-scroll-component"
+
 
 type Tweet = {
     id: string
@@ -16,5 +18,22 @@ type InfiniteTweetListProps = {
     tweets?: Tweet[]
 }
 export function InfiniteTweetList ({tweets,  isError, isLoading, fetchNewTweets, hasMore}: InfiniteTweetListProps) {
-    return <h1>tweets here</h1>
+    if(isLoading) return <h1>Loading..</h1>
+    if(isError) return <h1>Error...</h1>
+
+    if (tweets == null || tweets.length === 0) {
+        return <h2 className='my-4 text-center text-2x1 text-gray-500'></h2>
+    }
+
+    return <ul>
+        <InfiniteScroll
+        dataLength={tweets.length}
+        next={fetchNewTweets}
+        hasMore={hasMore}
+        loader={"Loading..."}>
+            {tweets.map(tweet => {
+                return <div key={tweet.id}>{tweet.content}</div>
+            })}
+        </InfiniteScroll>
+    </ul>
 }
