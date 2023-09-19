e5409ce6dd1020251d6b6e9c732a0e1c6fc416ea
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-19 16:42:05 -0700

    creating tweet card details

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -40,20 +40,25 @@ export function InfiniteTweetList ({tweets,  isError, isLoading, fetchNewTweets,
         </InfiniteScroll>
     </ul>)
 }
+const dateTimeFormatter = Intl.DateTimeFormat(undefined, { dateStyle: "short"})
 
 function TweetCard({id, user, content, createdAt, likeCount, likedByMe}: Tweet) {
     return <li className='flex gap-4 border-b px-4 py-4'>
         <Link href={`/profiles/${user.id}`}>
-            <ProfileImage src={user.image}/></Link>
+            <ProfileImage src={user.image}/>
+        </Link>
             <div className="flex flex-grow flex-col">
                 <div className='flex gap-1'>
                     <Link 
                         href={`/profiles/${user.id}`}
-                        className="font-bold hover:underline"
+                        className="font-bold outline-none hover:underline focus-visable:underline"
                     >
                     {user.name}
                     </Link>
+                    <span className="text-gray-500">-</span>
+                    <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
                 </div>
+                <p className="whitespace-pre-wrap">{content}</p>
             </div>
             </li>
 }
