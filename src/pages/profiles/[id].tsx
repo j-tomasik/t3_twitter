de99824eadc914821e316b034dd91903bb15f6d4
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-28 17:11:47 -0700

    query profile info from id passed up from server

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,18 +1,28 @@
-import {GetStaticPropsContext, NextPage} from 'next';
+import type {GetStaticPropsContext, NextPage, GetStaticPaths, InferGetStaticPropsType} from 'next';
 import Head from 'next/head';
 import { ssgHelper } from '~/server/api/ssgHelper';
+import { api } from '~/utils/api';
+import ErrorPage from 'next/error';
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+    id,
+}) => {
+    const {data: profile} = api.profile.getById.useQuery({id})
+
+    if(profile == null || profile.name == null) return <ErrorPage statusCode={404}/>
     return <>
         <Head>
-            <title>{`Twitter Clone ${user.name}`}</title>
+            <title>{`Twitter Clone ${profile.name}`}</title>
         </Head>
     
     </>
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
-    
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
 }
 
 export async function getStaticProps(context: GetStaticPropsContext<{id : string}>) {
